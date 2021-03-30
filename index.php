3b53a890a009676608789161316b66617f2a8975
MattW-R <Matthew.wr89@gmail.com>
2021-03-30 13:53:13 +0100

    Styled page with fonts

--- index.php
+++ index.php
@@ -12,13 +12,15 @@ $games = getGames($db);
 <html lang="en-GB">
     <head>
         <title>Matt's Board Game Collection</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu&family=Yatra+One&display=swap" rel="stylesheet" />
         <link href="normalize.css" type="text/css" rel="stylesheet" />
         <link href="styles.css" type="text/css" rel="stylesheet" />
     </head>
     <body>
         <nav>
             <h1>Matt's Board Game Collection</h1>
-            <button>ADD GAME</button>
+            <button>Add Game</button>
         </nav>
         <main>
             <?= displayGames($games); ?>
