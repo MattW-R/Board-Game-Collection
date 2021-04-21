273da3a626ca4683832e5bb89058cd5da4dfdf55
MattW-R <Matthew.wr89@gmail.com>
2021-04-21 16:42:48 +0100

    Added functionality to fetchAndDisplayGame to display game info as HTML using handlebars

--- functions.js
+++ functions.js
@@ -1,11 +1,23 @@
 const fetchAndDisplayGame = (bggId) => {
-    fetch('https://boardgamegeek.com/xmlapi2/thing?id=' + bggId)
-        .then(boardGameData => boardGameData.text())
-        .then(xmlString => {
-            let parser = new DOMParser()
-            return parser.parseFromString(xmlString, 'text/xml')
+    fetch('boardGamesTemplate.hbs')
+        .then(templateData => templateData.text())
+        .then(templateString => Handlebars.compile(templateString))
+        .then(template => {
+            fetch(`https://boardgamegeek.com/xmlapi2/thing?id=${bggId}&stats=1`)
+                .then(boardGameData => boardGameData.text())
+                .then(xmlString => {
+                    let parser = new DOMParser()
+                    return parser.parseFromString(xmlString, 'text/xml')
+                })
+                .then(xml => xml2json(xml, ''))
+                .then (jsonString => JSON.parse(jsonString))
+                .then (object => {
+                    document.getElementById(bggId).innerHTML = template(object)
+                })
         })
-        .then(xml => xml2json(xml, ''))
-        .then (jsonString => JSON.parse(jsonString))
-        .then (json => console.log(json))
+
+
 }
+
+// DEBUG
+fetchAndDisplayGame('259996')
