933826242d4f685923b9406baf6085d914604803
MattW-R <Matthew.wr89@gmail.com>
2021-04-22 09:47:51 +0100

    Refactored fetchAndDisplayGame to fetchGame & displayGame

--- functions.js
+++ functions.js
@@ -1,28 +1,30 @@
-const fetchAndDisplayGame = (bggId) => {
+const fetchGame = (bggId, callback) => {
+    fetch(`https://boardgamegeek.com/xmlapi2/thing?id=${bggId}&stats=1`)
+        .then(boardGameData => boardGameData.text())
+        .then(xmlString => {
+            let parser = new DOMParser()
+            return parser.parseFromString(xmlString, 'text/xml')
+        })
+        .then(xml => xml2json(xml, ''))
+        .then (jsonString => JSON.parse(jsonString))
+        .then (callback)
+}
+
+const displayGame = (gameObject) => {
     fetch('boardGamesTemplate.hbs')
         .then(templateData => templateData.text())
         .then(templateString => Handlebars.compile(templateString))
         .then(template => {
-            fetch(`https://boardgamegeek.com/xmlapi2/thing?id=${bggId}&stats=1`)
-                .then(boardGameData => boardGameData.text())
-                .then(xmlString => {
-                    let parser = new DOMParser()
-                    return parser.parseFromString(xmlString, 'text/xml')
-                })
-                .then(xml => xml2json(xml, ''))
-                .then (jsonString => JSON.parse(jsonString))
-                .then (object => {
-                    object.items.item.statistics.ratings.average['@value'] = parseFloat(object.items.item.statistics.ratings.average['@value']).toFixed(1)
-                    object.items.item.statistics.ratings.averageweight['@value'] = parseFloat(object.items.item.statistics.ratings.averageweight['@value']).toFixed(2)
-                    object.items.item.description = object.items.item.description.replaceAll('&amp;', '&')
-                    // document.getElementById(bggId).innerHTML = template(object)
+            gameObject.items.item.statistics.ratings.average['@value'] = parseFloat(gameObject.items.item.statistics.ratings.average['@value']).toFixed(1)
+            gameObject.items.item.statistics.ratings.averageweight['@value'] = parseFloat(gameObject.items.item.statistics.ratings.averageweight['@value']).toFixed(2)
+            gameObject.items.item.description = gameObject.items.item.description.replaceAll('&amp;', '&')
+            // document.getElementById(bggId).innerHTML = template(object)
 
-                    // DEBUG
-                    console.log(object)
-                    document.querySelectorAll('article')[0].innerHTML = template(object)
-                })
+            // DEBUG
+            console.log(gameObject)
+            document.querySelectorAll('article')[0].innerHTML = template(gameObject)
         })
 }
 
 // DEBUG
-fetchAndDisplayGame('259996')
+fetchGame('259996', displayGame)
